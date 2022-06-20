a4c60ab62930ca43a2d08218a356548d57341712
jhihsheng <b91202047@gmail.com>
2022-06-20 20:56:58 +0800

    docs

--- source/conf.py
+++ source/conf.py
@@ -27,8 +27,8 @@ author = 'JSW'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+# extensions = ["sphinx_rtd_dark_mode"]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -44,7 +44,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'piccolo_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
